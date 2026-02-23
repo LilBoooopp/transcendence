7f3c8df644d3f6a3532995d50398071818dbaca2
LilBoooopp <cbopp@student.42lausanne.ch>
2026-02-23 10:24:45 +0100

    tile component

---
+++ frontend/src/components/chessgui/Tile.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+interface TileProps {
+  rank: number
+  file: number
+  piece?: string
+  isHighlighted: boolean
+  theme: Record<string, string>
+  onClick: () => void
+}
+
+const Tile = (props: TileProps) => {
+  const { rank, file } = props
+  const isLight = (rank + file) % 2 !== 0
+
+  return (
+    <div 
+      onClick={props.onClick}
+      style={{
+        backgroundColor: props.isHighlighted ? 'yellow' : isLight ? '#f0d9b5' : '#b58863'
+      }}
+    >
+      {props.piece && <Piece type={props.piece} theme={props.theme}/>}
+    </div>
+  )
+}
+
+export default Tile
