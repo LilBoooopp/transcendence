9e40eee52ae0c0cc291e47fd959106a6812ac699
LilBoooopp <cbopp@student.42lausanne.ch>
2026-02-24 20:14:48 +0100

    working!

--- frontend/src/components/chessgui/Board.tsx
+++ frontend/src/components/chessgui/Board.tsx
@@ -15,42 +15,44 @@ interface BoardProps {
 
 const Board = (props: BoardProps) => {
   const fileToLetter = (file: number) => String.fromCharCode(file + 97)
+  const toActual = (rank: number, file: number) => ({
+    rank: props.playerColor === 'black' ? 7 - rank : rank,
+    file: props.playerColor === 'black' ? 7 - file : file,
+  })
   const handleDrop = (fromRank: number, fromFile: number, toRank: number, toFile: number) => {
-    const actualFromRank = props.playerColor === 'black' ? 7 - fromRank : fromRank
-    const actualFromFile = props.playerColor === 'black' ? 7 - fromFile : fromFile
-    const actualToRank = props.playerColor === 'black' ? 7 - toRank : toRank
-    const actualToFile = props.playerColor === 'black' ? 7 - toFile : toFile
-    const from = `${fileToLetter(actualFromFile)}${8 - actualFromRank}`
-    const to = `${fileToLetter(actualToFile)}${8 - actualToRank}`
-    props.onDrop(from, to)
+    const from = toActual(fromRank, fromFile)
+    const to = toActual(toRank, toFile)
+    props.onDrop(
+      `${fileToLetter(from.file)}${8 - from.rank}`,
+      `${fileToLetter(to.file)}${8 - to.rank}`
+    )
   }
-  const displayBoard = props.playerColor === 'black' ? [...props.board].reverse().map(row => [...row].reverse()) : props.board
-  const displayHighlighted = props.playerColor === 'black' ? [...props.highlighted].reverse().map(row => [...row].reverse()) : props.highlighted
+  const displayBoard = props.playerColor === 'black'
+    ? [...props.board].reverse().map(row => [...row].reverse())
+    : props.board
+  const displayHighlighted = props.playerColor === 'black'
+    ? [...props.highlighted].reverse().map(row => [...row].reverse())
+    : props.highlighted
   return (
     <DndProvider backend={HTML5Backend}>
       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(8, 1fr)' }}>
         {displayBoard.map((rank, rankIndex) =>
-          rank.map((piece, fileIndex) =>
-            <Tile
-              key={`${rankIndex}-${fileIndex}`}
-              rank={rankIndex}
-              file={fileIndex}
-              piece={piece ?? undefined}
-              theme={props.theme}
-              isHighlighted={displayHighlighted[rankIndex][fileIndex]}
-              onDrop={handleDrop}
-              onDragStart={() => {
-                const actualRank = props.playerColor === 'black' ? 7 - rankIndex: rankIndex
-                const actualFile = props.playerColor === 'black' ? 7 - fileIndex: fileIndex
-                props.onDragStart(actualRank, actualFile)
-              }}
-              onClick={() => {
-              const actualRank = props.playerColor === 'black' ? 7 - rankIndex : rankIndex
-              const actualFile = props.playerColor === 'black' ? 7 - fileIndex : fileIndex
-              props.onTileClick(actualRank, actualFile)
-              }}
-            />
-          )
+          rank.map((piece, fileIndex) => {
+            const { rank: actualRank, file: actualFile } = toActual(rankIndex, fileIndex)
+            return (
+              <Tile
+                key={`${rankIndex}-${fileIndex}`}
+                rank={rankIndex}
+                file={fileIndex}
+                piece={piece ?? undefined}
+                theme={props.theme}
+                isHighlighted={displayHighlighted[rankIndex][fileIndex]}
+                onDrop={handleDrop}
+                onDragStart={() => props.onDragStart(actualRank, actualFile)}
+                onClick={() => props.onTileClick(actualRank, actualFile)}
+              />
+            )
+          })
         )}
       </div>
     </DndProvider>
