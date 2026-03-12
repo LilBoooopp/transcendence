1c4be20b1e05c8f1b3f42525591367007de878c9
mrbes25 <bastian123schmid@gmail.com>
2026-03-12 21:10:10 +0100

    added profile page with profile info and editing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import WireframeDashboard from './pages/User/Dashboard';
 import WireframeLanding from './pages/Home/Landing';
 import WireframeGameMode from './pages/Game/GameMode';
 import WireframeBotMode from './pages/Game/BotMode';
+import ProfilePage from './pages/User/ProfilePage';
 
 import MatchmakingWaiting from './pages/Game/MatchmakingWaiting';
 import BotGameLauncher from './components/BotGameLauncher';
@@ -20,6 +21,7 @@ function App() {
         <Route path="/gamemode" element={<WireframeLayout><WireframeGameMode /></WireframeLayout>} />
         <Route path="/botmode" element={<WireframeLayout><WireframeBotMode /></WireframeLayout>} />
         <Route path="/dashboard" element={<WireframeLayout><WireframeDashboard /></WireframeLayout>} />
+				<Route path="/user" element={<WireframeLayout><ProfilePage /></WireframeLayout>} />
 
         <Route path="/play" element={<MatchmakingWaiting />} />
         <Route path="/bot-launch" element={<BotGameLauncher />} />
