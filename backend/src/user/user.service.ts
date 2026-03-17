fc5633f3f5b14e2075251deccab7bb3a8d429542
Sylvie Forster <sforster@c1r8s3.42lausanne.ch>
2026-03-17 23:47:28 +0100

    check pull request

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -223,19 +223,6 @@ export class UserService {
         if (!ok) {
             throw new UnauthorizedException('Invalid password');
         }
-    return {
-      username: user.username,
-      avatarUrl: user.avatarUrl,
-      memberSince,
-      totalGames: user.statistics?.totalGames ?? 0,  // ← petit t
-      avgScore,
-      bulletRating: user.statistics?.bulletElo,
-      blitzRating: user.statistics?.blitzElo,
-      rapidRating: user.statistics?.rapidElo,
-			currentStreak: user.statistics?.currentStreak,
-			bestStreak: user.statistics?.bestStreak
-    };
-  }
 
         const hashed = await bcrypt.hash(newPassword, 10);
 
